732886e522432ae7eace9034bf50b434a0301935
fqsocite <denispenzinn@gmail.com>
2026-07-20 14:41:18 +0200

    add in App.jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import { Psychologists } from '../src/pages/Psychologists/Psychologists'
+import { Home } from '../src/pages/Home/Home'
+import { Favorites } from '../src/pages/Favorites/Favorites'
 function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/psychologists" element={<Psychologists />}></Route>
+            <Route path="/favorites" element={ <Favorites />}></Route>
+        </Routes>
+    )
 }
 
 export default App
